a755717c5ba8c27377c4b7440165dc45b4d7fb69
john8739 <jsolly@esri.com>
2020-06-08 17:54:59 -0700

    Refactoring!

--- get_funcs.py
+++ get_funcs.py
@@ -13,6 +13,16 @@ ADMIN_GIS_OBJS = MySecrets.get_admin_gis_objs()
 TR_CLIENT_OBJ = MySecrets.get_test_rail_api_obj()
 
 
+def get_test_case_from_id(test_case_id: int) -> list:
+    for _ in range(5):  # Would be good to capsulize this logic
+        try:
+            test_case = TR_CLIENT_OBJ.send_get(uri=f"get_case/{test_case_id}")
+            return test_case
+        except APIError:
+            print("I got throttled!")
+            time.sleep(60)
+
+
 def get_suites_from_project_id() -> list:
     suites = TR_CLIENT_OBJ.send_get(uri=f"get_suites/{TESTRAIL_DICT['TR_PROJECT_ID']}")
     return suites
@@ -38,43 +48,3 @@ def get_test_cases_from_suite_id(suite_id: int) -> list:
         except APIError:
             print("I got throttled!")
             time.sleep(60)
-
-
-def get_test_case_from_id(test_case_id: int) -> list:
-    for _ in range(5):  # Would be good to capsulize this logic
-        try:
-            test_case = TR_CLIENT_OBJ.send_get(uri=f"get_case/{test_case_id}")
-            return test_case
-        except APIError:
-            print("I got throttled!")
-            time.sleep(60)
-
-
-def get_links_from_string(string):  # Change to match other ragexes
-    url_pattern = re.compile(
-        "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-    )
-    urls = url_pattern.findall(string)
-    return urls
-
-
-def get_item_ids_from_string(string):
-    item_id_pattern = re.compile("[0-9a-f]{32}")
-    item_ids = item_id_pattern.findall(string)
-    return item_ids
-
-
-def get_item_from_item_id(item_id):  # todo: implement a closure
-
-    for GIS in ADMIN_GIS_OBJS:
-        item = GIS.content.get(item_id)
-        if item:
-            return item
-
-    return False
-
-
-def get_item_host_name(item_obj):
-    item_url = item_obj.homepage
-    host_name = urlparse(item_url).hostname
-    return host_name
--- unittests/test_get_funcs.py
+++ unittests/test_get_funcs.py
@@ -20,39 +20,3 @@ class FirstClass(unittest.TestCase):
         test_case_id = "76247"
         test_cases = get_funcs.get_test_case_from_id(test_case_id)
         self.assertIsInstance(test_cases, dict)
-
-    def test_get_links_from_string(self):
-        string = "strings https://arcgis.com and more strings"
-        link = get_funcs.get_links_from_string(string)
-        self.assertEqual(
-            link, "https://arcgis.com"
-        )  # Is this testing a constant? Bad prtactice?
-
-    def test_get_item_ids_from_string(self):
-        string = """
-                Sample webmap with all geom and refresh types:
-                id: a540748151b84753b609d67488be363e
-                org: dbqa in devext
-                Sample dashboard
-                id: d54f71aadaca4cd1899e133bdf3c028c
-                org: dbqa in devext
-                """
-        item_ids = get_funcs.get_item_ids_from_string(string)
-        actual_item_ids = [
-            "a540748151b84753b609d67488be363e",
-            "d54f71aadaca4cd1899e133bdf3c028c",
-        ]
-
-        self.assertEqual(item_ids, actual_item_ids)
-
-    def test_get_item_from_item_id(self):
-        item_id = "a540748151b84753b609d67488be363e"
-        item = get_funcs.get_item_from_item_id(item_id)
-        self.assertIsInstance(item, Item)
-
-    def test_get_item_host_name(self):
-        item = AUTOMATION_DEVEXT_DBQA_GIS.content.get(
-            "ec18963f29864b7baf5f5eb236f6a545"
-        )
-        item_host_name = get_funcs.get_item_host_name(item)
-        self.assertEqual(item_host_name, AGOL_DICT["AGOL_DBQA_DEVEXT_HOST_NAME"])
